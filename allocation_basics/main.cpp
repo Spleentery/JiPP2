d9be9a1d5a5596e792093280dbe7dea05cd26035
david <david78787878787@gmail.com>
2021-10-12 21:22:56 +0200

    wdu- trojwymiarowa tablicy

--- allocation_basics/main.cpp
+++ allocation_basics/main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int rozmiar;
-    cout << "podaj rozmiar tablicy dynamicznej" << endl;
-    cin >> rozmiar;
-    int * array = new int[rozmiar];
-    for (int i = 0; i < rozmiar; ++i) {
-        cout << "x" << i << ":";
-        cin >> array[i] ;
+
+   cout << "two dimension array\n";
+   int array[2][2];
+   array[0][0] = 1;
+   array[0][1] = 2;
+   array[1][0] = 3;
+   array[1][1] = 4;
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            cout << array[i][j] << endl;
+        }
     }
-    cout << "your data in array is:\n";
-    for (int i = 0; i < rozmiar; ++i) {
-        cout << array[i] << endl;
+
+    cout << "tablica trojwymiarowa\n";
+    srand(time(0));
+    int ARRAY[2][2][2];
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            for (int k = 0; k < 2; ++k) {
+                 ARRAY[i][j][k] = 1 + rand() % 100;
+                 cout << ARRAY[i][j][k] << endl;
+            }
+
+        }
+
     }
-    delete[] array;
     return 0;
 }
