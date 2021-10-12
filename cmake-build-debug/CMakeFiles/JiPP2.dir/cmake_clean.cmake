38ad809c5ae693c0ee7d3ef6b64bc7599cb330df
david <david78787878787@gmail.com>
2021-10-12 11:28:42 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/JiPP2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JiPP2.dir/main.cpp.obj"
+  "JiPP2.exe"
+  "JiPP2.exe.manifest"
+  "JiPP2.lib"
+  "JiPP2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JiPP2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
