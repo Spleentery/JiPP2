6fb851ee15263ede1d11bd24e46c98e871c6e145
david <david78787878787@gmail.com>
2021-10-12 12:19:13 +0200

    dynamic array

--- allocation_basics/main.cpp
+++ allocation_basics/main.cpp
@@ -1,31 +1,18 @@
 #include <iostream>
 using namespace std;
 int main() {
-    string J = "JiPP2";
-    string *wsk = &J;
-    //cout << *wsk << endl;
-    //cout << wsk << endl;  // adress of variable J
-    //cout << &wsk << endl; // adress of pointer wsk
-    //cout << &J << endl;   // adress of variable J
-    for (int i = 0; i < 5; ++i) {
-        cout << *wsk << endl;
+    int rozmiar;
+    cout << "podaj rozmiar tablicy dynamicznej" << endl;
+    cin >> rozmiar;
+    int * array = new int[rozmiar];
+    for (int i = 0; i < rozmiar; ++i) {
+        cout << "x" << i << ":";
+        cin >> array[i] ;
     }
-    int table[5] = {1,2,3,4,5};
-    for (int i = 0; i < 5; ++i) {
-        cout << table[i] << endl;
+    cout << "your data in array is:\n";
+    for (int i = 0; i < rozmiar; ++i) {
+        cout << array[i] << endl;
     }
-    int * array = new int[3];
-    array[0] = 666;
-    array[1] = 999;
-    array[2] = 1000;
-    array[10] = 1090;
-    delete [] array;
-    cout << array[10] << endl;
-    cout << table << endl; // cout pinter to table
-    cout << *table;        // cout table[0]
-
-
-
-
+    delete[] array;
     return 0;
 }
