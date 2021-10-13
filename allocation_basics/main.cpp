fc96137c357155d7b463a1aa93e1396e964c8b1e
david <david78787878787@gmail.com>
2021-10-13 15:59:46 +0200

    Sum of arguments given in the terminal

--- allocation_basics/main.cpp
+++ allocation_basics/main.cpp
@@ -1,31 +1,15 @@
 #include <iostream>
+#include "stdlib.h"
 using namespace std;
-int main() {
 
-   cout << "two dimension array\n";
-   int array[2][2];
-   array[0][0] = 1;
-   array[0][1] = 2;
-   array[1][0] = 3;
-   array[1][1] = 4;
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 2; ++j) {
-            cout << array[i][j] << endl;
-        }
+int main(int argc, char *argv[]) {
+
+   int sum = 0;
+    for(int i = 0; i < argc ; ++i) {
+        cout << "argument[" << i << "] = " << argv[i] <<endl;
+        sum += atoi(argv[i]);
     }
+    cout << "sum:" << sum << endl;
 
-    cout << "tablica trojwymiarowa\n";
-    srand(time(0));
-    int ARRAY[2][2][2];
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 2; ++j) {
-            for (int k = 0; k < 2; ++k) {
-                 ARRAY[i][j][k] = 1 + rand() % 100;
-                 cout << ARRAY[i][j][k] << endl;
-            }
-
-        }
-
-    }
     return 0;
 }
